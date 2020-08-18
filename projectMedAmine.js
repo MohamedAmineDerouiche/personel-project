fccd1c14d1e5ef9d5a84b517ba06bd153d48549c
MohamedAmineDerouiche <mohderouiche250881@gmail.com>
2020-08-18 13:15:16 +0100

    personel project done

--- projectMedAmine.js
+++ projectMedAmine.js
@@ -1,12 +1,21 @@
 
 $("document").ready(function(){
-
+$('.welcome').hide();
 
   $("#loginUser").click(function(){
     $(".container").hide();
-    $('.welcome').show();
+    $('.welcome').show().css('font-size', '56px');
 
     
   })
    })
+var date = new Date;
 
+
+$('#loginUser').click (function(){
+	$('#enj').css('color', '#40E0D0').css('font-size', '75px');
+})
+
+$(`<span>${date}</span>`).appendTo("#saladimage");
+
+//location.replace("https://www.w3schools.com")
